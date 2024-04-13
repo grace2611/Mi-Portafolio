dd579ca9d7e813b477cc352501f5e69def47e4a5
grace_ur <uturuncoramirezgrace@gmail.com>
2024-04-13 11:42:20 -0500

    first commit

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ function closeModal() {
     modal.style.display = "none";
   }, 300); // Espera a que termine la animación para ocultar el modal
 }
+document.addEventListener("DOMContentLoaded", function() {
+    var footerContent = document.querySelector('.footer-content');
+    footerContent.classList.add('show');
+  });
+  
