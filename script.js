41c5769dc705276472b3e617f700569b95d9d1ca
grace_ur <uturuncoramirezgrace@gmail.com>
2024-04-13 11:14:58 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var modal = document.getElementById("myModal");
+
+function openModal() {
+  modal.style.display = "block";
+  modal.classList.remove("fade-out");
+  modal.classList.add("fade-in");
+}
+
+function closeModal() {
+  modal.classList.remove("fade-in");
+  modal.classList.add("fade-out");
+  setTimeout(function() {
+    modal.style.display = "none";
+  }, 300); // Espera a que termine la animación para ocultar el modal
+}
